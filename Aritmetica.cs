c10b3e2752da012c6f91c6c5ec35377e721a7b4b
Jorge C. Silva <254529958+jormfcsilva-cyber@users.noreply.github.com>
2026-01-31 11:23:16 +0000

    Adicionar método ConversaoTemperatura()

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -12,6 +12,15 @@ namespace Calculos
     internal class Aritmetica
     {
         /// <summary>
+        /// Tipo de conversão de temperatura a executar.
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusParaFahrenheit,
+            FahrenheitParaCelsius
+        }
+        /// <summary>
         /// Operação de soma entre dois números inteiros.
         /// </summary>
         /// <returns>Retorna a soma dos dois números.</returns>
@@ -27,6 +36,17 @@ namespace Calculos
         {
             return a - b;
         }
-
+        public double ConverterTemperatura(double valor, ConversaoTemperatura tipoConversao)
+        {
+            switch (tipoConversao)
+            {
+                case ConversaoTemperatura.CelsiusParaFahrenheit:
+                    return (valor * 9 / 5) + 32;
+                case ConversaoTemperatura.FahrenheitParaCelsius:
+                    return (valor - 32) * 5 / 9;
+                default:
+                    throw new ArgumentException("Tipo de conversão inválido.");
+            }
+        }
     }
 }
