44d39bc380854acdfdbfd780e5bd0d252000a10b
Jorge C. Silva <254529958+jormfcsilva-cyber@users.noreply.github.com>
2026-01-31 11:48:45 +0000

    Criar classe Conversoes e mover método
    O método ConverterTemperatura() removido da classe Aritmetica e colocado na classe Conversoes uma vez que implementa uma operação que não se adequa à classe.

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -11,15 +11,7 @@ namespace Calculos
     /// </summary>
     internal static class Aritmetica
     {
-        /// <summary>
-        /// Tipo de conversão de temperatura a executar.
-        /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo = 0,
-            CelsiusParaFahrenheit,
-            FahrenheitParaCelsius
-        }
+      
         /// <summary>
         /// Operação de soma entre dois números inteiros.
         /// </summary>
@@ -35,18 +27,6 @@ namespace Calculos
         public static int Subtrair(int a, int b)
         {
             return a - b;
-        }
-        public static double ConverterTemperatura(double valor, ConversaoTemperatura tipoConversao)
-        {
-            switch (tipoConversao)
-            {
-                case ConversaoTemperatura.CelsiusParaFahrenheit:
-                    return (valor * 9 / 5) + 32;
-                case ConversaoTemperatura.FahrenheitParaCelsius:
-                    return (valor - 32) * 5 / 9;
-                default:
-                    throw new ArgumentException("Tipo de conversão inválido.");
-            }
         }
     }
 }
---
+++ Conversoes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    /// <summary>
+    /// Implementa operações de conversão.
+    /// </summary>  
+    internal static class Conversoes
+    {
+        /// <summary>
+        /// Tipo de conversão de temperatura a executar.
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusParaFahrenheit,
+            FahrenheitParaCelsius
+        }
+        /// <summary>
+        /// Conversão de temperaturas.
+        /// </summary>  
+        /// <param name="conversao">A conversão a efetuar.</param>
+        /// <param name="temperatura">A temperatura a converter.</param>
+        /// <returns>Retorna o resultado da conversão</returns>
+        
+        public static double ConverterTemperatura(double valor, ConversaoTemperatura tipoConversao)
+        {
+            switch (tipoConversao)
+            {
+                case ConversaoTemperatura.CelsiusParaFahrenheit:
+                    return (valor * 9 / 5) + 32;
+                case ConversaoTemperatura.FahrenheitParaCelsius:
+                    return (valor - 32) * 5 / 9;
+                default:
+                    throw new ArgumentException("Tipo de conversão inválido.");
+            }
+        }
+    }
+}
